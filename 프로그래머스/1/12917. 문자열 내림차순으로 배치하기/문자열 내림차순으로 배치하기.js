6c3c7096e222a43866981070097bf13b49596fac
Melonisjy <79883658+Melonisjy@users.noreply.github.com>
2024-11-11 17:37:56 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,7 +1,3 @@
 function solution(s) {
-    // 문자열을 배열로 변환하고, 아스키 코드 값을 기준으로 내림차순 정렬
-    let arr = s.split("").sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0));
-    
-    // 배열을 다시 문자열로 변환하여 반환
-    return arr.join("");
+    return s.split("").sort().reverse().join("");
 }
